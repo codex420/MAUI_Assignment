82a332ea5161af7544319802f9b846c2b4ca8d93
Aanchal Jain <3100aaja@nibe.se>
2026-07-15 20:25:57 +0530

    Project Created

---
+++ Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MAUI_Assignment
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MAUI_Assignment
+{
+    // All the code in this file is only included on Android.
+    public class PlatformClass1
+    {
+    }
+}
