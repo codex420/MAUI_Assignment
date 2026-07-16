de03a205860a93185b7e20edbf8b8cf711e7d28b
Aanchal Jain <3100aaja@nibe.se>
2026-07-16 17:06:29 +0530

    color updated

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -4,6 +4,11 @@ using MAUI_Assignment.ViewModels;
 using MAUI_Assignment.Views;
 using Microsoft.Extensions.Logging;
 
+#if WINDOWS
+using WinUIBrush = Microsoft.UI.Xaml.Media.SolidColorBrush;
+using WinUIColors = Microsoft.UI.Colors;
+#endif
+
 namespace MAUI_Assignment;
 
 public static class MauiProgram
@@ -31,6 +36,29 @@ public static class MauiProgram
 
 #if DEBUG
         builder.Logging.AddDebug();
+#endif
+
+#if WINDOWS
+        Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping("CustomPickerDropdown", (handler, view) =>
+        {
+            var whiteBrush = new WinUIBrush(WinUIColors.White);
+            var blackBrush = new WinUIBrush(WinUIColors.Black);
+            
+            // Set ComboBox popup dropdown background
+            handler.PlatformView.Resources["ComboBoxDropDownBackground"] = whiteBrush;
+            handler.PlatformView.Resources["ComboBoxDropDownBackgroundPointerOver"] = whiteBrush;
+            
+            // Set ComboBox item text foreground colors
+            handler.PlatformView.Resources["ComboBoxItemForeground"] = blackBrush;
+            handler.PlatformView.Resources["ComboBoxItemForegroundPointerOver"] = blackBrush;
+            handler.PlatformView.Resources["ComboBoxItemForegroundSelected"] = blackBrush;
+            handler.PlatformView.Resources["ComboBoxItemForegroundSelectedPointerOver"] = blackBrush;
+
+            // Set ComboBox item background hover states
+            handler.PlatformView.Resources["ComboBoxItemBackgroundPointerOver"] = new WinUIBrush(Windows.UI.Color.FromArgb(255, 240, 240, 240));
+            handler.PlatformView.Resources["ComboBoxItemBackgroundSelected"] = new WinUIBrush(Windows.UI.Color.FromArgb(255, 230, 230, 230));
+            handler.PlatformView.Resources["ComboBoxItemBackgroundSelectedPointerOver"] = new WinUIBrush(Windows.UI.Color.FromArgb(255, 220, 220, 220));
+        });
 #endif
 
         return builder.Build();
