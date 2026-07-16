34c9c6f9a621b0ff5dde96e2feb8063f08a78669
Aanchal Jain <3100aaja@nibe.se>
2026-07-16 16:54:16 +0530

    icons added

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -17,6 +17,7 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("segmdl2.ttf", "SegoeMDL2");
             });
 
         // Services
